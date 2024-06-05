cdb34ff0ed7ff1014bcd8496b64aff6514bfd20b
ROHIT YADAV <163042643+DerHarteCoder@users.noreply.github.com>
2024-06-05 18:55:22 +0530

    check 4

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,12 +2,20 @@
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas} from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import {CanvasLoader} from '../Loader';
+// import {CanvasLoader} from '../Loader.jsx';
+// import { HemisphereLight } from 'three';
 
 const Computers = () => {
+const computer = useGLTF('./public/desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black'/>
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
   )
 }
 
 export default Computers
+
