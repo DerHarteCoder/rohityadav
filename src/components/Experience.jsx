1437266659d6867a3a0c122c42715970da4ac5ee
ROHIT YADAV <163042643+DerHarteCoder@users.noreply.github.com>
2024-06-18 12:23:14 +0530

    bbb

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,14 +17,14 @@ const Experience = () => {
       <h2 className={styles.sectionHeadText}>Work Experience.</h2>
     </motion.div>
  
-    {/* <div className='mt-20 flex flex-col'>
+    
+{/* <div className='mt-20 flex flex-col'>
      <VerticalTimeline>
       {experiences.map((experience, index) => (
         <ExperienceCard key={index} experience={experience}/>
       ))}
      </VerticalTimeline>
     </div> */}
-
     </>
   )
 }
